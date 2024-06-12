91e8f77c0dbed81a3ca82f7b0c4a43aa55d4e5e5
Atul kumar Kashyap <1234atulkashyap@gmail.com>
2024-06-12 12:11:22 +0530

    Added new project-VideoMeet

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,19 +3,41 @@ import React from "react";
 import proj1 from "../assets/proj1.png";
 import proj2 from "../assets/proj2.png";
 import proj3 from "../assets/proj3.png";
+import proj4 from "../assets/proj4.png";
 
 const projects = [
-  { id: 1, img: proj1, title: "Youtube Clone", liveUrl: "https://youtube-clone-iota-two.vercel.app/" },
-  { id: 2, img: proj2, title: "CarHub", liveUrl: "https://car-hub-gamma-gules.vercel.app/" },
-  { id: 2, img: proj3, title: "Portfolio", liveUrl: "https://my-portfolio-eta-indol.vercel.app/" },
+  {
+    id: 1,
+    img: proj1,
+    title: "Youtube Clone",
+    liveUrl: "https://youtube-clone-iota-two.vercel.app/",
+  },
+  {
+    id: 2,
+    img: proj2,
+    title: "CarHub",
+    liveUrl: "https://car-hub-gamma-gules.vercel.app/",
+  },
+  {
+    id: 3,
+    img: proj3,
+    title: "Portfolio",
+    liveUrl: "https://my-portfolio-eta-indol.vercel.app/",
+  },
+  {
+    id: 4,
+    img: proj4,
+    title: "Video-Meet",
+    liveUrl: "https://video-meet-eta.vercel.app",
+  },
 ];
 
 const Work = () => {
-
-
-
   return (
-    <div className="mx-auto max-w-[1200px] grid grid-cols-2 gap-8 glass p-6 m-5" id="work">
+    <div
+      className="mx-auto max-w-[1200px] grid grid-cols-2 gap-8 glass p-6 m-5"
+      id="work"
+    >
       <div className="col-span-2 my-2 md:my-4">
         <h1 className="text-4xl mb-2 md:mb-4 text-gray-300">My work</h1>
       </div>
@@ -25,7 +47,7 @@ const Work = () => {
             key={items.id}
             className="glass  transition-all   hover:scale-105  duration-300 overflow-hidden shadow-xl   md:h-[200px] relative group text-green-400 "
           >
-            <div className="hover:blur-[1px] transition-all duration-300"   >
+            <div className="hover:blur-[1px] transition-all duration-300">
               <img
                 className="w-full h-full object-cover "
                 src={items.img}
@@ -38,6 +60,7 @@ const Work = () => {
               <span className="text-2xl font-bold">{items.title}</span>
               <div>
                 <a
+                  target="_blank"
                   className="bg-white px-5 py-2 rounded-xl font-bold"
                   href={items.liveUrl}
                 >
@@ -51,8 +74,5 @@ const Work = () => {
     </div>
   );
 };
-
-
-
 
 export default Work;
